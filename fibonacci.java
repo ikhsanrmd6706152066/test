1173929aad5d71017ad8ee1a1ddb2d07a99c8068
Ikhsan Ramadhan_6706152066 <ikhsanrmd27@gmail.com>
2017-04-07 09:39:29 +0700

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class fibonacci{
+    public static void main (String args[]){
+        int[] bil=new int[100];
+        bil[0]=0;
+        bil[1]=1;        
+        int deret;
+        
+        Scanner input = new Scanner (System.in);
+        System.out.print("Input : ");
+        String data = input.nextLine();
+        deret = Integer.parseInt(data);
+        
+        for(int i=0;i<deret;i++){
+            System.out.print(bil[i]+" ");
+            bil[i+2]=bil[i]+bil[i+1];
+        }
+    }
+} 
